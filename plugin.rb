d9257270e53790d433f9bb4c9a41be09b43d48f0
Daniel Stadelman <bioradmeister@gmail.com>
2015-07-22 11:57:32 -0600

    Added URL to the plugin.rb file so Discourse interface would have a link to the project page

--- plugin.rb
+++ plugin.rb
@@ -1,7 +1,8 @@
 # name: embed-allowall
 # about: Allow all hosts to embed discourse comments
 # version: 1
-# authors: dstadelman, riking
+# authors: Daniel Stadelman, riking
+# url: https://github.com/ruffbytes/embed-allowall
 
 Rails.application.config.action_dispatch.default_headers.merge!({'X-Frame-Options' => 'ALLOWALL'})
 
