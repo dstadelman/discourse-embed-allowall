22ecc5b8ab3ca9152d5072a76bef93eb1ff052c9
Daniel Stadelman <ruffbytes@users.noreply.github.com>
2015-10-05 13:43:30 -0600

    Changed github URL

--- plugin.rb
+++ plugin.rb
@@ -1,8 +1,7 @@
-# name: embed-allowall
+# name: discourse-embed-allowall
 # about: Allow all hosts to embed discourse comments
 # version: 1
 # authors: Daniel Stadelman, riking
-# url: https://github.com/ruffbytes/embed-allowall
+# url: https://github.com/ruffbytes/discourse-embed-allowall
 
 Rails.application.config.action_dispatch.default_headers.merge!({'X-Frame-Options' => 'ALLOWALL'})
-
